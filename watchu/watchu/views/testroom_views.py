64106ac682fa6de546d1a2ad2407ca21ba3527bc
seungmin <nolzaheo@gmail.com>
2021-07-15 15:07:37 +0900

    branch 정리중

--- watchu/watchu/views/testroom_views.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-bp = Blueprint('testroom', __name__, url_prefix='/testroom')
-
-@bp.route('/menu')
-def menu():
-    return render_template('testroom/menu.html')
