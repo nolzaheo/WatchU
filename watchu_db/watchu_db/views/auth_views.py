dab875838427e06429d8d50649dba998f2b07da5
ririhan <45311765+RIANAEH@users.noreply.github.com>
2021-08-04 10:53:36 +0900

    필요한 페이지 모두 작성
    이미지 불러오기, 로그 불러오기, 부정행위 실시간 알림 구현 필요!!

--- watchu_db/watchu_db/views/auth_views.py
+++ watchu_db/watchu_db/views/auth_views.py
@@ -1,3 +1,5 @@
+import functools
+
 from flask import Blueprint, url_for, render_template, flash, request, session, g
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import redirect
@@ -7,31 +9,20 @@ from watchu_db.models import Professor
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-@bp.route('/')
-def login():
-    return render_template('/auth/login_professor.html')
-
-@bp.route('/signup_professor/', methods=('GET', 'POST'))
-def signup_professor():
-    ''' 교수 회원가입 '''
-    form = ProfessorCreateForm()
-    if request.method == 'POST' and form.validate_on_submit():
-        professor = Professor.query.filter_by(id=form.id.data).first()
-        if not professor:
-            professor = Professor(
-                id=form.id.data,
-                password=generate_password_hash(form.password1.data)
-            )
-            db.session.add(professor)
-            db.session.commit()
-            return redirect(url_for('auth.login_professor'))
-        else:
-            flash('이미 존재하는 교수입니다.')
-    return render_template('/auth/signup_professor.html', form=form)
+def login_not_required(view):
+    """ 로그인 여부 확인 데코레이터 """
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is not None:
+            return redirect(url_for('test_room.menu'))
+        return view(**kwargs)
+    return wrapped_view
 
 @bp.route('/login_professor/', methods=('GET', 'POST'))
+@bp.route('/')
+@login_not_required
 def login_professor():
-    ''' 교수 로그인'''
+    """ 교수 로그인"""
     form = ProfessorLoginForm()
     if request.method == 'POST' and form.validate_on_submit():
         error = None
@@ -47,6 +38,25 @@ def login_professor():
         flash(error)
     return render_template('auth/login_professor.html', form=form)
 
+@bp.route('/signup_professor/', methods=('GET', 'POST'))
+@login_not_required
+def signup_professor():
+    """ 교수 회원가입 """
+    form = ProfessorCreateForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        professor = Professor.query.filter_by(id=form.id.data).first()
+        if not professor:
+            professor = Professor(
+                id=form.id.data,
+                password=generate_password_hash(form.password1.data)
+            )
+            db.session.add(professor)
+            db.session.commit()
+            return redirect(url_for('auth.login_professor'))
+        else:
+            flash('이미 존재하는 교수입니다.')
+    return render_template('/auth/signup_professor.html', form=form)
+
 @bp.before_app_request
 def load_logged_in_professor():
     professor_id = session.get('professor_id')
@@ -57,6 +67,17 @@ def load_logged_in_professor():
 
 @bp.route('/logout_professor/')
 def logout_professor():
-    ''' 교수 로그아웃 '''
+    """ 교수 로그아웃 """
     session.clear()
     return redirect(url_for('auth.login_professor'))
+
+def login_required(view):
+    """ 로그인 여부 확인 데코레이터 """
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login_professor'))
+        return view(**kwargs)
+    return wrapped_view
+
+
--- watchu_db/watchu_db/views/test_room_views.py
+++ watchu_db/watchu_db/views/test_room_views.py
@@ -1,41 +1,190 @@
-import json
+import base64
+from datetime import datetime
+from tempfile import TemporaryFile
 
-from flask import Blueprint, render_template, request, jsonify
+from flask import Blueprint, render_template, request, jsonify, g, url_for, Response
+from werkzeug.datastructures import FileStorage
+from werkzeug.utils import redirect
+
+from watchu_db.models import Professor, TestRoom, Student, Log
+from watchu_db import db
 import secrets
+
+from watchu_db.views import auth_views
 
 bp = Blueprint('test_room', __name__, url_prefix='/test_room')
 
 @bp.route('/')
 @bp.route('/menu')
+@auth_views.login_required
 def menu():
     return render_template('test_room/menu.html')
 
-@bp.route('/make')
+@bp.route('/make', methods=["GET"])
+@auth_views.login_required
 def make():
     test_room_id = secrets.token_urlsafe(11)
     print(test_room_id)
-    return render_template('test_room/make.html')
+    return render_template('test_room/make.html', test_room_id=test_room_id)
 
-@bp.route('/upload_image', methods=["POST"])
-def upload_image():
-    if request.method == 'POST':
-        images = request.files.getlist("images[]")
-        print(images)
-        for img in images:
-            img.save('./images/' + img.filename)
-            print(img.filename.split('.')[0])
-        return render_template('test_room/menu.html')
+@bp.route('/make_ajax', methods=['GET', 'POST'])
+def make_ajax():
+    test_room_id = request.form['test_room_id']
+    professor_id = request.form['professor_id']
+    block_list = request.form['block_list']
 
-@bp.route('/ajax', methods=["POST"])
-def ajax():
-    data = json.loads(request.data)
-    block_list = data['block_list']
-    image_name = data['student_list']
-    image_list = data['image_list']
-    print(data)
-    print(block_list)
-    print(image_name)
-    # print(image_list)
+    date = request.form['date'].split('-')
+    start = request.form['start_time'].split(':')
+    end = request.form['end_time'].split(':')
+    start_date = datetime(int(date[0]), int(date[1]), int(date[2]), int(start[0]), int(start[1]))
+    end_date = datetime(int(date[0]), int(date[1]), int(date[2]), int(end[0]), int(end[1]))
+    # db에 TestRoom, Student 객체 생성 및 저장
+    test_room = TestRoom(
+        id=test_room_id,
+        professor=Professor.query.get(professor_id),
+        block_list=block_list,
+        start_date=start_date, end_date=end_date)
+    db.session.add(test_room)
+    length = request.form['length']
+    for i in range(int(length)):
+        image = request.files[str(i)]
+        print(type(image))
+        print(image)
+        id = image.filename[:8]
+        image_data = image.read()
+        print(type(image), type(image_data))
+        student = Student(id=id, test_room_id=test_room_id, image=image_data)
+        db.session.add(student)
+    db.session.commit()
     return "ajax"
+
+@bp.route('/edit_list', methods=["GET"])
+@auth_views.login_required
+def edit_list():
+    professor = Professor.query.get(g.user.id)
+    test_room_list = []
+    for t in professor.test_room_set:
+        test_room_list.append(t.id)
+    return render_template('test_room/edit_list.html', test_room_list=test_room_list)
+
+import io
+from PIL import Image
+
+@bp.route('/edit/<string:test_room_id>', methods=["GET"])
+@auth_views.login_required
+def edit(test_room_id):
+    test_room = TestRoom.query.get(test_room_id)
+    block_list = test_room.block_list.split(';')[:-1]
+    start_date = str(test_room.start_date)
+    end_date = str(test_room.end_date)
+    date = start_date[0:10]
+    start_time = start_date[11:16]
+    end_time = end_date[11:16]
+    student_list = []
+    imageBuffer = []
+    for s in test_room.student_set:
+        student_list.append(s.id)
+        image = base64.b64encode(s.image).decode()
+        imageBuffer.append(image)
+        # print(base64.b64encode(s.image))
+        # img = Image.open(io.BytesIO(s.image))
+        # img.filename = str(s.id) + ".jpg"
+        # fp = TemporaryFile()
+        # img.save(fp, "JPEG")
+        # imageBuffer.append(fp)
+        # fp.close()
+
+        # image = FileStorage(stream=s.image, filename=str(s.id) + ".jpg", content_type='image/jpeg')
+        # imageBuffer.append(image)
+
+        # image = open(str(s.id) + '.jpg', mode='wb')
+        # image.write(s.image)
+        # imageBuffer.append(image)
+
+        # print(image.name)
+        # image.close()
+    # print(imageBuffer)
+    return render_template(f'test_room/edit.html', test_room_id=test_room_id, date=date, start_time=start_time, end_time=end_time, block_list=block_list, student_list=student_list, imageBuffer=imageBuffer)
+
+@bp.route('/edit_ajax', methods=['GET', 'POST'])
+def edit_ajax():
+    test_room_id = request.form['test_room_id']
+    block_list = request.form['block_list']
+
+    date = request.form['date'].split('-')
+    start = request.form['start_time'].split(':')
+    end = request.form['end_time'].split(':')
+    start_date = datetime(int(date[0]), int(date[1]), int(date[2]), int(start[0]), int(start[1]))
+    end_date = datetime(int(date[0]), int(date[1]), int(date[2]), int(end[0]), int(end[1]))
+
+    # db에 TestRoom, Student 객체 수정 및 추가
+    test_room = TestRoom.query.get(test_room_id)
+    test_room.block_list = block_list
+    test_room.start_date = start_date
+    test_room.end_date = end_date
+    # db.session.add(test_room)
+    length = request.form['length']
+    for i in range(int(length)):
+        image = request.files[str(i)]
+        id = image.filename[:8]
+        image_data = image.read()
+        # image_data.replace("b&#39;", "")
+        # image_data.replace("&#39;", "")
+        # print(str(image_data))
+        print(image_data[:100])
+        student = Student.query.filter(Student.id == id, Student.test_room_id == test_room_id).first()
+        if student != None:
+            student.image = image_data
+        else:
+            student = Student(id=id, test_room_id=test_room_id, image=image_data)
+        # db.session.add(student)
+    # db.session.commit()
+    return "ajax"
+
+@bp.route('/delete/<string:test_room_id>', methods=["GET"])
+def delete(test_room_id):
+    """ 시험 삭제 """
+    test_room = TestRoom.query.get(test_room_id)
+    db.session.delete(test_room)
+    db.session.commit()
+    return redirect(url_for('test_room.edit_list'))
+
+@bp.route('/watching_list', methods=["GET"])
+@auth_views.login_required
+def watching_list():
+    professor = Professor.query.get(g.user.id)
+    test_room_list = []
+    for t in professor.test_room_set:
+        test_room_list.append(t.id)
+    return render_template('test_room/watching_list.html', test_room_list=test_room_list)
+
+image = open('./static/no_screen.png', 'rb').read()
+images = dict()
+
+def gen_frames(student_id):
+    while True:
+        # image = images[student_id]
+        frame = bytearray(image)
+        yield (b'--frame\r\n'
+                b'Content-Type: image/png\r\n\r\n' + frame + b'\r\n')
+
+@bp.route('/<string:test_room_id>/<int:student_id>/screen_socket_feed')
+def screen_socket_feed(test_room_id, student_id):
+    return Response(gen_frames(student_id), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@bp.route('/watching/<string:test_room_id>', methods=["GET"])
+@auth_views.login_required
+def watching(test_room_id):
+    test_room = TestRoom.query.get(test_room_id)
+    student_list = []
+    for s in test_room.student_set:
+        student_list.append(s.id)
+        # images[s.id] = s.image # 임시로 학생 사진 넣기
+    print(test_room_id, student_list)
+    return render_template('test_room/watching.html', test_room_id=test_room_id, student_list=student_list)
+
+@bp.route('/watching_detail/<string:test_room_id>/<int:student_id>')
+def watching_detail(test_room_id, student_id):
+    return render_template('test_room/watching_detail.html', test_room_id=test_room_id, student_id=student_id)
 
 
