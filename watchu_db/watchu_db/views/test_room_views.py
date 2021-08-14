d995c5785377592f362fb7cf38e2cbe89d1e808f
ririhan <45311765+RIANAEH@users.noreply.github.com>
2021-08-14 19:43:44 +0900

    전체 기능 구현 완료

--- watchu_db/watchu_db/views/test_room_views.py
+++ watchu_db/watchu_db/views/test_room_views.py
@@ -14,6 +14,9 @@ from watchu_db.views import auth_views
 bp = Blueprint('test_room', __name__, url_prefix='/test_room')
 
 
+program_list = ["크롬", "마이크로소프트 엣지", "파이어폭스", "사파리", "카카오톡", "메모장", "계산기"]
+
+
 @bp.route('/', methods=["GET"])
 @bp.route('/menu', methods=["GET"])
 @auth_views.login_required
@@ -26,8 +29,7 @@ def menu():
 def make():
     """ 시험 생성 """
     test_room_id = secrets.token_urlsafe(11)
-    print(test_room_id)
-    return render_template('test_room/make.html', test_room_id=test_room_id)
+    return render_template('test_room/make.html', test_room_id=test_room_id, program_list=program_list)
 
 
 @bp.route('/make_ajax', methods=['GET', 'POST'])
@@ -41,13 +43,14 @@ def make_ajax():
     end = request.form['end_time'].split(':')
     start_date = datetime(int(date[0]), int(date[1]), int(date[2]), int(start[0]), int(start[1]))
     end_date = datetime(int(date[0]), int(date[1]), int(date[2]), int(end[0]), int(end[1]))
+    title = request.form['title']
 
     # TestRoom 객체 생성
     test_room = TestRoom(
         id=test_room_id,
         professor=Professor.query.get(professor_id),
         block_list=block_list,
-        start_date=start_date, end_date=end_date)
+        start_date=start_date, end_date=end_date, title=title, state="before")
     db.session.add(test_room)
 
     # Student 객체 생성
@@ -69,7 +72,7 @@ def edit_list():
     professor = Professor.query.get(g.user.id)
     test_room_list = []
     for t in professor.test_room_set:
-        test_room_list.append(t.id)
+        test_room_list.append((t.id, t.state, t.title, str(t.start_date)[:16], str(t.end_date)[:16]))
     return render_template('test_room/edit_list.html', test_room_list=test_room_list)
 
 
@@ -78,6 +81,7 @@ def edit_list():
 def edit(test_room_id):
     """ 시험 수정 """
     test_room = TestRoom.query.get(test_room_id)
+    title = test_room.title
     block_list = test_room.block_list.split(';')[:-1]
     start_date = str(test_room.start_date)
     end_date = str(test_room.end_date)
@@ -93,8 +97,12 @@ def edit(test_room_id):
         image_list.append(img)
     print(student_list)
     print(len(image_list))
-    return render_template(f'test_room/edit.html', test_room_id=test_room_id, date=date, start_time=start_time,
-                           end_time=end_time, block_list=block_list, student_list=student_list, image_list=image_list)
+    block_list_ = []
+    for i in range(len(program_list)):
+        if program_list[i] in block_list:
+            block_list_.append(i)
+    return render_template(f'test_room/edit.html', test_room_id=test_room_id, title=title, program_list=program_list, date=date, start_time=start_time,
+                           end_time=end_time, block_list=block_list, block_list_=block_list_, student_list=student_list, image_list=image_list)
 
 
 @bp.route('/edit_ajax', methods=['GET', 'POST'])
@@ -102,33 +110,35 @@ def edit_ajax():
     """ 시험 수정 정보 받기 & 데이터베이스 변경 """
     test_room_id = request.form['test_room_id']
     block_list = request.form['block_list']
-
+    title = request.form['title']
     date = request.form['date'].split('-')
     start = request.form['start_time'].split(':')
     end = request.form['end_time'].split(':')
     start_date = datetime(int(date[0]), int(date[1]), int(date[2]), int(start[0]), int(start[1]))
     end_date = datetime(int(date[0]), int(date[1]), int(date[2]), int(end[0]), int(end[1]))
 
-    # TestRoom 객체 수정
+    # TestRoom 객체 삭제 후 다시 생성
+    professor_id = TestRoom.query.get(test_room_id).professor_id
     test_room = TestRoom.query.get(test_room_id)
-    test_room.block_list = block_list
-    test_room.start_date = start_date
-    test_room.end_date = end_date
+    db.session.delete(test_room)
+    db.session.commit()
+    test_room = TestRoom(
+        id=test_room_id,
+        professor=Professor.query.get(professor_id),
+        block_list=block_list,
+        start_date=start_date, end_date=end_date, title=title, state="before")
     db.session.add(test_room)
 
-    # Student 객체 수정 및 삭제
+    # Student 객체 생성
     length = request.form['length']
     for i in range(int(length)):
         img = request.files[str(i)]
         student_id = img.filename[:8]
         image_data = img.read()
-        student = Student.query.filter(Student.id == student_id, Student.test_room_id == test_room_id).first()
-        if student is not None:
-            student.image = image_data
-        else:
-            student = Student(id=student_id, test_room_id=test_room_id, image=image_data)
+        student = Student(id=student_id, test_room_id=test_room_id, image=image_data)
         db.session.add(student)
     db.session.commit()
+    print("Edit Ajax Success")
     return "Edit Ajax Success"
 
 
@@ -148,36 +158,78 @@ def watching_list():
     professor = Professor.query.get(g.user.id)
     test_room_list = []
     for t in professor.test_room_set:
-        test_room_list.append(t.id)
+        test_room_list.append((t.id, t.state, t.title, str(t.start_date)[:16], str(t.end_date)[:16]))
     return render_template('test_room/watching_list.html', test_room_list=test_room_list)
+
+
+@bp.route('/watching_before/<string:test_room_id>', methods=["GET"])
+@auth_views.login_required
+def watching_before(test_room_id):
+    """ 시험 시작 전 화면  """
+    test_room = TestRoom.query.get(test_room_id)
+    student_list = []
+    for s in test_room.student_set:
+        student_list.append(s.id)
+    return render_template('test_room/watching_before.html', test_room_id=test_room_id, title=test_room.title, start_date=str(test_room.start_date)[:16], end_date=str(test_room.end_date)[:16], student_list=student_list)
 
 
 no_screen_image = open('./static/no_screen.png', 'rb').read()
 global_images = dict()  # 전체 화면 공유 이미지 저장 딕셔너리
 
 
+@bp.route('/start/<string:test_room_id>', methods=["GET"])
+@auth_views.login_required
+def start(test_room_id):
+    """ 시험 시작 """
+    # 화면 공유 이미지 딕셔너리 생성
+    test_room = TestRoom.query.get(test_room_id)
+    images = dict()
+    for s in test_room.student_set:
+        images[s.id] = no_screen_image
+    global_images[test_room_id] = images
+
+    # state를 watching으로 변경
+    test_room = TestRoom.query.get(test_room_id)
+    test_room.state = "watching"
+    db.session.add(test_room)
+    db.session.commit()
+
+    print(f"=== {test_room_id}번 시험 시작!! ===")
+    return redirect(url_for('test_room.watching', test_room_id=test_room_id))
+
+
 @bp.route('share_screen/<string:test_room_id>/<int:student_id>', methods=['POST'])
 def share_screen(test_room_id, student_id):
     """ HTTP로 화면 받기 """
-    image = base64.b64decode(request.form["image"])
-    global_images[test_room_id][student_id] = image
-    img = Image.open(io.BytesIO(image))
-    img.save("MyTest1.png")
-    return "done!!"
+    test_room = TestRoom.query.get(test_room_id)
+    state = test_room.state
+    # 화면 이미지 저장
+    if state == "watching":
+        image = base64.b64decode(request.form["image"])
+        global_images[test_room_id][student_id] = image
+
+    # 시험 상태 보내기
+    json = dict()
+    json["state"] = state
+    return jsonify(json)
 
 
 def gen_frames(test_room_id, student_id):
     while True:
-        image = global_images[test_room_id][student_id]
-        print(student_id, global_images[test_room_id][student_id][:100])
+        test_room = TestRoom.query.get(test_room_id)
+        state = test_room.state
+        # 화면 이미지 저장
+        if state == "watching":
+            image = global_images[test_room_id][student_id] # 왜 에러??
+        else:
+            image = no_screen_image
         frame = bytearray(image)
-        img = Image.open(io.BytesIO(image))
-        img.save("MyTest2.png")
         yield (b'--frame\r\n'
                b'Content-Type: image/png\r\n\r\n' + frame + b'\r\n')
 
 
 @bp.route('/screen_socket_feed/<string:test_room_id>/<int:student_id>')
+@auth_views.login_required
 def screen_socket_feed(test_room_id, student_id):
     return Response(gen_frames(test_room_id, student_id), mimetype='multipart/x-mixed-replace; boundary=frame')
 
@@ -185,16 +237,12 @@ def screen_socket_feed(test_room_id, student_id):
 @bp.route('/watching/<string:test_room_id>', methods=["GET"])
 @auth_views.login_required
 def watching(test_room_id):
-    """ 시험 중 화면 공유 """
+    """ 시험 감독 중 화면 공유 """
     test_room = TestRoom.query.get(test_room_id)
     student_list = []
-    images = dict()
     for s in test_room.student_set:
         student_list.append(s.id)
-        images[s.id] = no_screen_image
-    global_images[test_room_id] = images
-    print(test_room_id, student_list)
-    return render_template('test_room/watching.html', test_room_id=test_room_id, student_list=student_list)
+    return render_template('test_room/watching.html', test_room_id=test_room_id, title=test_room.title, start_date=str(test_room.start_date)[:16], end_date=str(test_room.end_date)[:16], student_list=student_list)
 
 
 watching_log_buffer = set()  # 부정행위가 감지 된 학생 번호
@@ -202,11 +250,12 @@ watching_log_buffer = set()  # 부정행위가 감지 된 학생 번호
 
 @bp.route('watching/log_ajax/<string:test_room_id>', methods=['POST'])
 def watching_log_ajax(test_room_id):
-    """ 시험 중 부정행위 알림 """
+    """ 시험 감독 중 부정행위 알림 """
     json_list = []
     json_list.append({"length": len(watching_log_buffer)})
     if len(watching_log_buffer) != 0:
         for student_id in watching_log_buffer:
+            print(f"부정행위 {student_id}")
             json_list.append({"student_id": student_id})
         watching_log_buffer.clear()
         return jsonify(json_list)
@@ -214,10 +263,19 @@ def watching_log_ajax(test_room_id):
         return jsonify(json_list)
 
 
+@bp.route('watching/image_ajax/<string:test_room_id>', methods=['POST'])
+def watching_image_ajax(test_room_id):
+    json_list = []
+    for g in global_images[test_room_id]:
+        json_list.append({"student_id": g, "image": base64.b64encode(global_images[test_room_id][g]).decode()})
+    print("Image ajax Success")
+    return jsonify(json_list)
+
+
 @bp.route('/student_login/<string:test_room_id>/<int:student_id>', methods=['POST'])
 def student_login(test_room_id, student_id):
     """ 학생 로그인 (학생 이미지, 시험 시작, 종료, 차단 프로그램 전송) """
-    print(test_room_id, student_id)
+    print("학생 로그인", test_room_id, student_id)
     json = dict()
     test_room = TestRoom.query.get(test_room_id)
     if test_room is not None:
@@ -227,6 +285,10 @@ def student_login(test_room_id, student_id):
             json["student"] = "yes"
             # 학생 이미지 전송
             json["student_image"] = base64.b64encode(student.image).decode()
+            # 시험 제목 전송
+            json["title"] = test_room.title
+            # 시험 상태 전송
+            json["state"] = test_room.state
             # 시험 시작, 종료 전송
             json["start_date"] = str(test_room.start_date)
             json["end_date"] = str(test_room.end_date)
@@ -236,15 +298,13 @@ def student_login(test_room_id, student_id):
             json["student"] = "no"
     else:
         json["test_room"] = "no"
-    print(json)
     return jsonify(json)
 
 
 @bp.route('/log/<string:test_room_id>/<int:student_id>', methods=['POST'])
 def log(test_room_id, student_id):
     """ 로그 받기 """
-    print(test_room_id, student_id)
-    print(request.form["type"])
+    print("로그 받아옴", test_room_id, student_id, request.form["type"])
     type = request.form["type"]
     date = datetime.strptime(request.form["date"], '%Y-%m-%d %H:%M:%S')
     image = request.form["image"]
@@ -273,8 +333,9 @@ watching_detail_log_buffer = list()  # 부정행위 로그 내용 저장
 
 
 @bp.route('/watching_detail/<string:test_room_id>/<int:student_id>')
+@auth_views.login_required
 def watching_detail(test_room_id, student_id):
-    """ 시험 중 학생 디테일 (한명 화면 공유, 로그 띄우기) """
+    """ 시험 감독 중 학생 디테일 (한명 화면 공유, 로그 띄우기) """
     watching_detail_log_buffer.clear()
     log_list = []
     student = Student.query.filter(Student.id == student_id, Student.test_room_id == test_room_id).first()
@@ -297,8 +358,58 @@ def watching_detail_log_ajax(test_room_id, student_id):
     json_list = [{"length": len(watching_detail_log_buffer)}]
     if len(watching_detail_log_buffer) != 0:
         for log_data in watching_detail_log_buffer:
+            print("부정행위")
             json_list.append(log_data)
         watching_detail_log_buffer.clear()
+        watching_log_buffer.remove(student_id)
         return jsonify(json_list)
     else:
         return jsonify(json_list)
+
+
+@bp.route('/terminate/<string:test_room_id>', methods=["GET"])
+def terminate(test_room_id):
+    """ 시험 종료 """
+    # 화면 공유 이미지 딕셔너리 삭제
+    del global_images[test_room_id]
+    # global_images[test_room_id].clear()
+
+    # state를 result으로 변경
+    test_room = TestRoom.query.get(test_room_id)
+    test_room.state = "result"
+    db.session.add(test_room)
+    db.session.commit()
+    return redirect(url_for('test_room.watching_result', test_room_id=test_room_id))
+
+
+@bp.route('/watching_result/<string:test_room_id>')
+@auth_views.login_required
+def watching_result(test_room_id):
+    """ 시험 결과 """
+    test_room = TestRoom.query.get(test_room_id)
+    result_list = []
+    for s in test_room.student_set:
+        result_list.append((s.id, len(s.log_set)))
+    print(result_list)
+    result_list.sort(key=lambda x: (-x[1]))
+    print(result_list)
+    return render_template('test_room/watching_result.html', test_room_id=test_room_id, result_list=result_list)
+
+
+@bp.route('/watching_result_detail/<string:test_room_id>/<int:student_id>')
+@auth_views.login_required
+def watching_result_detail(test_room_id, student_id):
+    """ 시험 결과 학생 디테일 (로그 띄우기) """
+    log_list = []
+    student = Student.query.filter(Student.id == student_id, Student.test_room_id == test_room_id).first()
+    for log in student.log_set:
+        log_data = dict()
+        log_data["type"] = log.type
+        log_data["date"] = log.date
+        if log.image is None:
+            log_data["image"] = "None"
+        else:
+            log_data["image"] = base64.b64encode(log.image).decode()
+        log_list.append(log_data)
+    return render_template('test_room/watching_result_detail.html', test_room_id=test_room_id, student_id=student_id,
+                           log_list=log_list)
