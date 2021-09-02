5cf53f081ecf3768f2ced7e72f385c37e45d97b4
RIANAEH <ririhan217@gmail.com>
2021-09-03 02:21:41 +0900

    init 파일 변경

--- WatchU-Web/WatchU/__init__.py
+++ WatchU-Web/WatchU/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
-import config
 
 naming_convention = {
     "ix": 'ix_%(column_0_label)s',
